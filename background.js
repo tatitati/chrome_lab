79dc6ff2bdd9db07affc0b9948b4ff4394dfcf76
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-23 15:21:57 +0100

    butotn to change color

--- background.js
+++ background.js
@@ -6,16 +6,6 @@ chrome.runtime.onInstalled.addListener(function(){
 })
 
 chrome.bookmarks.onCreated.addListener(function(){
-    console.log('Bookmarks created')
-
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        console.log(tabs[0])
-        console.log(tabs[0].url)
-        
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: `document.body.style.backgroundColor = 'red'`}
-        )
-    })
+    console.log('Bookmarks created');    
 })
 
