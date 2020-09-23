4d06182dc8578ef1c7c8978c14bcac99e10d9b1f
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-23 13:11:02 +0100

    more

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
 chrome.runtime.onInstalled.addListener(function(){
-    console.log("just installed, hellow world")
+    console.log("just installed, hellow world")    
 })
 
 chrome.bookmarks.onCreated.addListener(function(){
     console.log('Bookmarks created')
+
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        console.log(tabs[0])
+        console.log(tabs[0].url)
+        
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: `document.body.style.backgroundColor = 'red'`}
+        )
+    })
 })
+
